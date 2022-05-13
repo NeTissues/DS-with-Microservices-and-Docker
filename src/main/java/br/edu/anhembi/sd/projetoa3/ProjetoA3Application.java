f92c92b521a6a14f7ae8e895d763bc703159d63f
Raphael Scandura <94921631+raphael-malltech@users.noreply.github.com>
2022-05-13 19:33:36 -0300

    Project structure

---
+++ src/main/java/br/edu/anhembi/sd/projetoa3/ProjetoA3Application.java
@@ -0,0 +1,13 @@
+package br.edu.anhembi.sd.projetoa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoA3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoA3Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/anhembi/sd/projetoa3/ProjetoA3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.anhembi.sd.projetoa3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoA3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
