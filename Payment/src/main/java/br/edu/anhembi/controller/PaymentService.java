2c97f6741212fa97e411b67a0330d000f0633342
NeTissues <thiago.morango.personal@gmail.com>
2022-06-01 18:51:55 -0300

    Payment Module Functional

---
+++ Payment/src/main/java/br/edu/anhembi/controller/PaymentService.java
@@ -0,0 +1,26 @@
+package br.edu.anhembi.controller;
+
+import br.edu.anhembi.model.Payment;
+import br.edu.anhembi.model.PaymentRegistrationRequest;
+import br.edu.anhembi.model.repository.PaymentRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    public void registerPayment(PaymentRegistrationRequest paymentRegistrationRequest){
+        Payment payment = Payment.builder()
+                .orderId(paymentRegistrationRequest.orderId())
+                .userId(paymentRegistrationRequest.userId())
+                .orderStatus(paymentRegistrationRequest.orderStatus())
+                .paymentMethod(paymentRegistrationRequest.paymentMethod())
+                .paymentTime(LocalDateTime.now())
+                .build();
+        paymentRepository.saveAndFlush(payment);
+    }
+}
---
+++ Payment/src/main/java/br/edu/anhembi/model/Payment.java
@@ -0,0 +1,28 @@
+package br.edu.anhembi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long orderId;
+    private Long userId;
+    private OrderStatus orderStatus;
+    private String paymentMethod;
+    private LocalDateTime paymentTime;
+}
